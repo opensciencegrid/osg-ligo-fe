d5b51a7285c640f6a0f5356c31836c5330444368
root <root@osg-ligo-1.t2.ucsd.edu>
2016-06-14 11:14:58 -0700

    Added the new gfal-check verification script

---
+++ scripts/node-check/gfal-check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#
+# Script for verifying that gfal-copy is available in a node
+#
+#
+# Created by Edgar on June 10 2016
+#
+#############################################
+#
+#
+#!/bin/sh
+
+glidein_config="$1"
+# find error reporting helper script
+error_gen=`grep '^ERROR_GEN_PATH ' $glidein_config | awk '{print $2}'`
+PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64/usr/bin:$PATH
+export PYTHONPATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64/usr/lib64/python2.6/site-packages:$PYTHONPATH
+export LD_LIBRARY_PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64/usr/lib64/:$LD_LIBRARY_PATH
+export X509_CERT_DIR=/cvmfs/oasis.opensciencegrid.org/mis/certificates
+export GFAL_PLUGIN_DIR=/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.2/current/el6-x86_64/usr/lib64/gfal2-plugins/
+export GFAL_CONFIG_DIR=/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.2/current/el6-x86_64/etc/gfal2.d/
+gfal-copy -vvv -f 'root://xrootd-local.unl.edu//user/ligo/test_access/access_ligo' file:///dev/null
+if [ $? == 0 ]; then
+    echo "gfal-copy succeed"
+    "$error_gen" -ok "gfal/copy"
+    exit 0
+else
+    "$error_gen" -error "gfal-copy" "WN_Resource" "gfal-copy failed" "file" "gfal-copy failed"
+    exit 1
+fi
---
+++ scripts/node-check/vsize-check.sh
@@ -0,0 +1,26 @@
+#                                                                                                    
+#                                                                                                    
+# Script for verifying that CVMFS is available in a node                                             
+#                                                                                                    
+#                                                                                                    
+# Created by Edgar on August 3 2015                                                                  
+#                                                                                                    
+#############################################                                                        
+#                                                                                                    
+#                                                                                                    
+#!/bin/sh                                                                                            
+glidein_config="$1"
+# find error reporting helper script                                                                 
+error_gen=`grep '^ERROR_GEN_PATH ' $glidein_config | awk '{print $2}'`
+vsize=`ulimit -Sv`
+if [ $vsize == "unlimited" ]; then
+  echo "Vsize unlimited"
+  "$error_gen" -ok "vsize"
+  exit 0
+fi
+if [ $vsize -le 3000000 ]; then 
+  echo "ulimit -sV less than 3000000"
+  "$error_gen" -error "ulimit -sV less than 3000000" "WN_Resource" "VSize too smal" "file" "vsizey"
+  exit 1
+fi
+exit 0
