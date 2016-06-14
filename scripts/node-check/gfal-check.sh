0ae3bf5707c823f0bd52fa744db7847a0e50f8ea
root <root@osg-ligo-1.t2.ucsd.edu>
2016-06-14 13:28:17 -0700

    Fixed the PYTHONPATH

--- scripts/node-check/gfal-check.sh
+++ scripts/node-check/gfal-check.sh
@@ -14,12 +14,13 @@
 glidein_config="$1"
 # find error reporting helper script
 error_gen=`grep '^ERROR_GEN_PATH ' $glidein_config | awk '{print $2}'`
-PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64/usr/bin:$PATH
-export PYTHONPATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64/usr/lib64/python2.6/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64/usr/lib64/:$LD_LIBRARY_PATH
+CVMFS_WN_PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64
+PATH=$CVMFS_WN_PATH/usr/bin:$PATH
+export PYTHONPATH=$CVMFS_WN_PATH/usr/lib/python2.6/site-packages:$CVMFS_WN_PATH/usr/lib64/python2.6/site-packages:$PYTHONPATH
+export LD_LIBRARY_PATH=$CVMFS_WN_PATH/usr/lib64/:$LD_LIBRARY_PATH
 export X509_CERT_DIR=/cvmfs/oasis.opensciencegrid.org/mis/certificates
-export GFAL_PLUGIN_DIR=/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.2/current/el6-x86_64/usr/lib64/gfal2-plugins/
-export GFAL_CONFIG_DIR=/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.2/current/el6-x86_64/etc/gfal2.d/
+export GFAL_PLUGIN_DIR=$CVMFS_WN_PATH/usr/lib64/gfal2-plugins/
+export GFAL_CONFIG_DIR=$CVMFS_WN_PATH/etc/gfal2.d/
 gfal-copy -vvv -f 'root://xrootd-local.unl.edu//user/ligo/test_access/access_ligo' file:///dev/null
 if [ $? == 0 ]; then
     echo "gfal-copy succeed"
