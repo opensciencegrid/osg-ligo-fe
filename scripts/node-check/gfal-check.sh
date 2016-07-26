688e3b003b5a8fb08bcf4e0ae799539e4dcf64b8
root <root@osg-ligo-1.t2.ucsd.edu>
2016-07-25 17:13:49 -0700

    Deal in a better way with el7 wn

--- scripts/node-check/gfal-check.sh
+++ scripts/node-check/gfal-check.sh
@@ -14,9 +14,24 @@
 glidein_config="$1"
 # find error reporting helper script
 error_gen=`grep '^ERROR_GEN_PATH ' $glidein_config | awk '{print $2}'`
-CVMFS_WN_PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64
+
+# Determine the release and set up the CVMFS_WN_PATH and PYTHONPATH accordingly
+release=`cat /etc/redhat-release`
+case $release in
+    *6.* )
+        CVMFS_WN_PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.2/current/el6-x86_64
+        export PYTHONPATH=$CVMFS_WN_PATH/usr/lib/python2.6/site-packages:$CVMFS_WN_PATH/usr/lib64/python2.6/site-packages:$PYTHONPATH
+        ;;
+    *7.* )
+        CVMFS_WN_PATH=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.3/current/el7-x86_64
+	export PYTHONPATH=$CVMFS_WN_PATH/usr/lib/python2.7/site-packages:$CVMFS_WN_PATH/usr/lib64/python2.7/site-packages:$PYTHONPATH
+        ;;
+    * )
+       echo 'Could not determine OS release'
+       exit 1
+       ;;
+esac
 PATH=$CVMFS_WN_PATH/usr/bin:$PATH
-export PYTHONPATH=$CVMFS_WN_PATH/usr/lib/python2.6/site-packages:$CVMFS_WN_PATH/usr/lib64/python2.6/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$CVMFS_WN_PATH/usr/lib64/:$LD_LIBRARY_PATH
 export X509_CERT_DIR=/cvmfs/oasis.opensciencegrid.org/mis/certificates
 export GFAL_PLUGIN_DIR=$CVMFS_WN_PATH/usr/lib64/gfal2-plugins/
