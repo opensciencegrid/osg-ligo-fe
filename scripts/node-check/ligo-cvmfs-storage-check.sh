12b3fff643c631230eccae86674897af1b0dd6e7
root <root@osg-ligo-1.t2.ucsd.edu>
2016-11-14 13:47:10 -0800

    Modified to always advertise

--- scripts/node-check/ligo-cvmfs-storage-check.sh
+++ scripts/node-check/ligo-cvmfs-storage-check.sh
@@ -14,10 +14,11 @@ function warn {
 }
 
 function advertise {
-    # atype is the type of the value as defined by GlideinWMS:                                                                                                                                    
-    #   I - integer                                                                                                                                                                               
-    #   S - quoted string                                                                                                                                                                         
-    #   C - unquoted string (i.e. Condor keyword or expression)                                                                                                                                   
+    # atype is the type of the value as defined by GlideinWMS:                                                                                                                               
+    #   I - integer                                                                                                                                                                          
+   #   S - quoted string                                                                                                                                                                 
+    #   C - unquoted string (i.e. Condor keyword or expression)                                                                                                                              
+    
     key="$1"
     value="$2"
     atype="$3"
@@ -46,13 +47,13 @@ info "In case of problems, contact Edgar Fajardo (emfajard@ucsd.edu)"
 
 info "Checking for CVMFS availability and attributes..."
 
-$FS=ligo.osgstorage.org
+FS=ligo.osgstorage.org
 RESULT="False"
-if [ -e /cvmfs/$FS/. ]; then
-    md5sum /cvmfs/$FS/test_access/access_ligo
-    if [ $? == 0 ]; then
-	RESULT="True"
-    fi
-    FS_ATTR="HAS_CVMFS_$FS_CONV"
-    advertise $FS_ATTR "$RESULT" "C"
+FS_ATTR="HAS_CVMFS_LIGO_STORAGE"
+md5sum /cvmfs/$FS/test_access/access_ligo
+if [ $? == 0 ]; then
+    RESULT="True"
 fi
+advertise $FS_ATTR "$RESULT" "C"
+##################                                                                                                                                                   
+info "All done - time to do some real work!"
