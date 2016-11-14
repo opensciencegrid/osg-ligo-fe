a60141eb1008a3414bbb290f2be0755b1ddececa
root <root@osg-ligo-1.t2.ucsd.edu>
2016-11-14 12:38:06 -0800

    Removed the LAL_DATA_PATH setting per Duncan request

--- scripts/job-wrappers/osg-job-wrapper.sh
+++ scripts/job-wrappers/osg-job-wrapper.sh
@@ -7,27 +7,3 @@ echo "Site: $GLIDEIN_Site" 1>&2
 export GLIDEIN_Id=`grep -i '^Name' $_CONDOR_MACHINE_AD`
 echo "PilotID: $GLIDEIN_Id" 1>&2
 
-# Handle LAL data
-LAL_DATA_PATH=$(readlink -f $LAL_DATA_PATH)
-# note [[ has different semantics from [ in bash
-if [[ "$LAL_DATA_PATH" == /cvmfs/* ]]; then
-  echo "Using CVMFS for LAL data" 1>&2
-elif [ -z "$LAL_DATA_PATH" ]; then
-  # do nothing
-  echo "$LAL_DATA_PATH is unset by job."
-else
-  # Create a copy of $LAL_DATA_PATH locally if it does not already exist.
-  if [ -d ../../lal_data ]; then
-    export LAL_DATA_PATH=`readlink -f ../../lal_data`
-  elif [ -d $LAL_DATA_PATH ]; then
-    TMP_LAL_DATA_PATH=`mktemp -d ../../lal_data.inprogress.XXXXXX`
-    cp -r "$LAL_DATA_PATH"/* "$TMP_LAL_DATA_PATH"
-    export LAL_DATA_PATH=`readlink -f ../../lal_data`
-    mv -T $TMP_LAL_DATA_PATH $LAL_DATA_PATH
-    if [ $? -ne 0 ]; then
-      rm -rf "$TMP_LAL_DATA_PATH"
-    fi
-  fi
-fi
-echo "Using $LAL_DATA_PATH for LAL_DATA_PATH" 1>&2
-
